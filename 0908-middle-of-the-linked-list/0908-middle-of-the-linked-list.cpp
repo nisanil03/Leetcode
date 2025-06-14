ca721b36ef7b6a51e95f4993526a70dbe700ecfd
Anil jaiswal <aniljaiswal2441@gmail.com>
2025-06-14 12:32:56 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.75%) - LeetHub

---
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) 
+    {
+        ListNode* slowPointer = head;
+        ListNode* fastPointer = head;
+
+        while(fastPointer != nullptr && fastPointer ->next != nullptr)
+        {
+            slowPointer = slowPointer -> next;
+            fastPointer = fastPointer ->next ->next;
+        }
+        return slowPointer;
+    }
+};
