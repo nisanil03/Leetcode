ff54272ba21fbfbc76117aba3309a7fe776a7ab1
Anil jaiswal <aniljaiswal2441@gmail.com>
2025-11-28 21:33:53 +0530

    Time: 169 ms (27.4%), Space: 191 MB (17.79%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& vals, int k) {
-        vector<vector<int>> graph(n);
-        vector<int> degree(n);
-        if (n < 2) return 1;
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int> > G(n);
+        for(auto edge : edges){
+            G[edge[0]].push_back(edge[1]);
+            G[edge[1]].push_back(edge[0]);
         }
-
-        vector<long long> nodeVal(vals.begin(), vals.end());
-        queue<int> leafQ;
-        for (int i = 0; i < n; i++) 
-            if (degree[i] == 1) leafQ.push(i);
-
-        int compCnt = 0;
-        while (!leafQ.empty()) {
-            int curr = leafQ.front();
-            leafQ.pop();
-            degree[curr]--;
-            long long carry = 0;
-            if (nodeVal[curr] % k == 0) compCnt++;
-            else carry = nodeVal[curr];
-            for (int nbr : graph[curr]) {
-                if (degree[nbr] == 0) continue;
-                degree[nbr]--;
-                nodeVal[nbr] += carry;
-                if (degree[nbr] == 1) leafQ.push(nbr);
+        stack <int> S;
+        vector<int> parent(n);
+        
+        auto DFS = [&](int u, int p, auto &&DFS) -> void{
+            parent[u] = p;
+            S.push(u);
+            for(auto v : G[u]){
+                if(v == p) continue;
+                DFS(v, u, DFS);
+            }
+        };
+        DFS(0, -1, DFS);
+        vector<long long> sum(n);
+        for(int i = 0; i < n; i ++){
+            sum[i] = values[i];
+        }
+        int ans = 0;
+        while(!S.empty()){
+            int u = S.top();
+            S.pop();
+            if(sum[u] % k == 0){
+                ans += 1;
+            }
+            else{
+                sum[parent[u]] += sum[u];
             }
         }
-        return compCnt;
+        return ans;
     }
 };
